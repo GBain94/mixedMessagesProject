e5c5bf2771406dfb435e1ef96e25de03cdf4afc0
GBain94 <skylergbain@gmail.com>
2024-08-21 19:34:47 +0100

    Add files via upload

---
+++ mixedMessage/MMcode.js
@@ -0,0 +1,18 @@
+
+let youAre = ["You are Cancer", "You are Taurus", "You are Sagittarius", "You are Virgo"];
+let whichMeans = ["You are sensitive", "You are stubborn", "You are adventurous", "You are analytical"];
+let conditional = ["unless other indicators contradict this", "which is amplified by your other placements"];
+ 
+
+
+let counter = 1;
+function printString() {
+do {
+    counter ++;
+    let randomElement1 = youAre[Math.floor(Math.random() * youAre.length)];
+    let randomElement2 = whichMeans[Math.floor(Math.random() * whichMeans.length)];
+    let randomElement3 = conditional[Math.floor(Math.random() * conditional.length)];
+    return document.getElementById("results").innerHTML = `${randomElement1}. ${randomElement2}, ${randomElement3}`; 
+  
+} while (counter < 99);
+};
